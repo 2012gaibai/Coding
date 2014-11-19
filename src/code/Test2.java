2a1b843c9a9fc5ddf2b7ec50c59c85c5e190df29
Administrator <Administrator@alvis-PC>
2014-11-19 17:53:31 +0800

    ti

--- src/code/Test2.java
+++ src/code/Test2.java
@@ -1,25 +1,30 @@
 package code;
 
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Test2 {
 	public static void main(String[] args) {
-		String [] to={"daxue513@126.com","1078429355@qq.com","21321.sad@qq"};
-		for(String tmp:to){
-			
+		double num = 0;
+		for (int i = 1; i < 6; i++) {
+			num +=(double) 1 / i;
 		}
+		num=num*1000000;
+		System.out.println(num);
+		int out = (int) num;
+		
+		System.out.println(out);
 	}
-	
-	private static boolean isEmail(String email){
-		if("".equals(email)||email==null){
+
+	private static boolean isEmail(String email) {
+		if ("".equals(email) || email == null) {
 			return false;
 		}
-		
-		Pattern p=Pattern.compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
-		Matcher m=p.matcher(email);
-		
+
+		Pattern p = Pattern
+				.compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
+		Matcher m = p.matcher(email);
+
 		return m.matches();
 	}
 }
