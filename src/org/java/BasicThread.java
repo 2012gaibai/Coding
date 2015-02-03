1298e6fa428d62394e5e69fb77c73464b25388b0
Administrator <Administrator@alvis-PC>
2015-02-03 09:37:03 +0800

    提交

---
+++ src/org/java/BasicThread.java
@@ -0,0 +1,9 @@
+package org.java;
+
+public class BasicThread {
+	public static void main(String[] args) {
+		Thread thread=new Thread(new LiftOff());
+		thread.start();
+		System.out.println("waiting for liftOff");
+	}
+}
---
+++ src/org/java/MainThread.java
@@ -0,0 +1,12 @@
+package org.java;
+
+public class MainThread {
+	public static void main(String[] args) {
+		//LiftOff liftOff = new LiftOff();
+		//liftOff.run();
+		for(int i=0;i<5;i++){
+			new LiftOff().run();
+		}
+		System.out.println("waiting for liftOff");
+	}
+}
