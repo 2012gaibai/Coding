fd5f285b4f80462dc9c7d7d4c4bc7f1369889f32
Administrator <Administrator@alvis-PC>
2015-02-11 16:56:13 +0800

    提交

---
+++ src/think/code/chapter3/train/MinMax.java
@@ -0,0 +1,15 @@
+package think.code.chapter3.train;
+
+public class MinMax {
+public static void main(String[] args) {
+	double maxd=Double.MAX_VALUE;
+	double mind=Double.MIN_VALUE;
+	System.out.println("Max Double "+maxd);
+	System.out.println("Min Double "+mind);
+	
+	float maxf=Float.MAX_VALUE;
+	float minf=Float.MIN_VALUE;
+	System.out.println("Max Float "+maxf);
+	System.out.println("Min Float "+minf);
+}
+}
