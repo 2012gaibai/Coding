680a8358fa21325a887efd53f436584614787830
Administrator <Administrator@alvis-PC>
2015-02-12 13:08:15 +0800

    tijiao

---
+++ src/think/code/chapter4/train/E01_To100.java
@@ -0,0 +1,9 @@
+package think.code.chapter4.train;
+
+public class E01_To100 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			System.out.print(i + " ");
+		}
+	}
+}
---
+++ src/think/code/chapter4/train/E03_RandomInts.java
@@ -0,0 +1,9 @@
+package think.code.chapter4.train;
+
+public class E03_RandomInts {
+	public static void main(String[] args) {
+		while(true){
+			E02_RandomInts.compareRand();
+		}
+	}
+}
